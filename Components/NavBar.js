8cd4f6d076ab5a3d7c19134d5f8a5aa274cc4c6f
lucasnicolaszalazar@hotmail.com <lucaszalazar@MBP-de-lucas.fibertel.com.ar>
2021-11-26 23:49:48 -0300

    commit

---
+++ Components/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return <nav>
+        <a href="#">Productos</a>
+        <a href="#">Servicio Tecnico</a>
+        <a href="#">Nosotros</a>
+    </nav>
+}
+
+export default NavBar;
