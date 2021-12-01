14a6f14ca7a79cad587ca00df804f42c9067a368
lucasnicolaszalazar@hotmail.com <lucaszalazar@MBP-de-lucas.fibertel.com.ar>
2021-11-30 21:10:35 -0300

    nuevo commit

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,8 +1,13 @@
+import CartWidget from "./CartWidget";
+import ItemListContainer from "./ItemListContainer";
+
 const NavBar = () => {
     return <nav>
         <a href="#">Productos</a>
         <a href="#">Servicio Tecnico</a>
         <a href="#">Nosotros</a>
+        <CartWidget/>
+        <ItemListContainer greeting="bienvenidos al sitio"/> 
     </nav>
 }
 
